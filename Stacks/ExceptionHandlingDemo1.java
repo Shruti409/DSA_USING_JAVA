443a8039d6283348ebf0090dfd4822eea65d4db4
Shruti409 <60936697+Shruti409@users.noreply.github.com>
2021-11-10 01:41:44 +0530

    Create ExceptionHandlingDemo1.java

---
+++ Stacks/ExceptionHandlingDemo1.java
@@ -0,0 +1,32 @@
+package ExceptionHandling;
+
+public class ExceptionDemo {
+
+	public static void assistant(int lines)
+	{
+		System.out.println("Suno employees ek project bna k do");
+		try {
+		employees(3000);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+			System.out.println("Chalo mai hi bna leta hu nhi to naukri gyi");
+		}
+	}
+	public static void  employees(int lines) throws Exception
+	{
+		if(lines>2500)
+		{
+		throw new Exception("Hum nhi krenge");
+		}
+	}
+	
+	public static void main(String[] args) { //supervisor
+		
+System.out.println("Suno assistant ek project bnwao");
+assistant(3000);
+System.out.println("Assistent bhai tera promotion");
+	}
+
+}
