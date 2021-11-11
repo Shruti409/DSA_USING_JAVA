6bffbafa21fdf6b7f0d899e3f6139a432347f259
Shruti409 <60936697+Shruti409@users.noreply.github.com>
2021-11-11 23:56:22 +0530

    Create IronManV2.java

---
+++ Stacks/IronManV2.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class IronManV2 extends IronManV1 {
+	String ai="Jarvis";
+	public void canFly()
+	{
+		System.out.println("I can fly");
+	}
+public void unibeam()
+{
+	System.out.println("i can throw unibeam");
+}
+	
+}
