c73769abd3d0ac22a5554432324ed35a513837cc
Shruti409 <60936697+Shruti409@users.noreply.github.com>
2021-11-11 23:55:27 +0530

    Create IronManV1.java

---
+++ Stacks/IronManV1.java
@@ -0,0 +1,26 @@
+package inheritance;
+
+public class IronManV1 {
+
+	int helmet=1;
+	int hands=2;
+	int legs=2;
+	String arcReactor="Palladium";
+	
+	public void canFly()
+	{
+		System.out.println("i cannot fly");
+	}
+	public final void missiles()
+	{
+		System.out.println("i am missiler thrower");
+	}
+	private void flameThrower()
+	{
+		System.out.println("i throw fire");
+	}
+	
+
+
+}
+
