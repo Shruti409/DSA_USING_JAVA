0d89a923ebb27a746b4767fc52a627d0b7f4cd7b
Shruti409 <60936697+Shruti409@users.noreply.github.com>
2021-11-13 00:29:21 +0530

    Create SuperConstructor2.java

---
+++ Stacks/SuperConstructor2.java
@@ -0,0 +1,38 @@
+package inheritance;
+class Car
+{
+	String company;
+
+	public Car(String comp)
+	{
+		company =comp;
+		
+		
+	}
+	public Car()
+	{
+		company ="xyz";
+	}
+}
+
+class Mercedes extends Car
+{
+	int price;
+	public Mercedes(int price)
+	{
+		super("abc");
+		this.price=price;
+		
+	}
+}
+public class SuperConstructor2 {
+	
+	public static void main(String[] args) {
+		Mercedes m=new Mercedes(20000000);
+		System.out.println(m.company);
+		
+		
+	}
+	
+
+}
